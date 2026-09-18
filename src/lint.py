69aaebf0f7c5023a744b46b623f6495f753d0435
manfred <manfred@ares.internal>
2026-09-18 10:25:32 +0200

    switch to pathlib for meta discovery

--- src/lint.py
+++ src/lint.py
@@ -7,33 +7,18 @@ from linter import Linter
 
 RULES_PATH = Path("./rules/")
 
-def getRuleDirs():
-    return [Path(x[0]) for x in os.walk(RULES_PATH)]
-
-def getRuleFiles(dir):
-    files = os.listdir(dir)
-    return [
-        Path(dir / f) for f in files if os.path.isfile(Path(dir / f))
-    ]
-
 def main():
     linter = Linter()
-    dirs = getRuleDirs()
-    for dir in dirs:
-        files = getRuleFiles(dir)
-        if len(files) < 2:
-            continue
-        if not Path(dir / "metadata.yml") in files:
-            print(f"[!] -> Could not find metadata.yaml in {dir}")
-            continue
-        for file in files:
-            if file.name == "metadata.yml":
-                print(f"[i] -> Validating {file}")
-                data = linter.load(file)
-                if linter.validate(data):
-                    print(f"[+] -> Validated {file}")
-                else:
-                    print(f"[!] -> Failed to validate {file}")
 
-if __name__ == "__main__":
+    files = RULES_PATH.rglob("metadata.yml")
+    for file in files:
+        if (len(os.listdir(Path(file.parent))) < 2):
+            continue
+        metadata = linter.load(file)
+        if linter.validate(metadata):
+            print(f"[+] -> Validated metadata [{file}]")
+        else:
+            print(f"[!] -> Failed to validate metadata [{file}]")
+
+   if __name__ == "__main__":
     main()
